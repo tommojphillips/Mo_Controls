3be4d71852e4d5e175c72a2f5dd322f5a941afbc
Tommo J. Armytage <tommojphillips@gmail.com>
2026-01-09 15:25:12 +1000

    # Updated mod to v2.0.1.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -14,16 +14,16 @@ using System.Resources;
 [assembly: NeutralResourcesLanguage("en-AU")]
 
 // Version information
-[assembly: AssemblyVersion("2.0.0.195")]
-[assembly: AssemblyFileVersion("2.0.0.195")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
 
 namespace TommoJProductions.MoControlsV2 {
 
     public class VersionInfo {
-	    public const string lastestRelease = "08.01.2026 03:18 PM";
-	    public const string version = "2.0.0";
-	    public const string full_version = "2.0.0.195";
-	    public const string build = "195";
+	    public const string lastestRelease = "09.01.2026 03:02 PM";
+	    public const string version = "2.0.1";
+	    public const string full_version = "2.0.1.0";
+	    public const string build = "0";
 
 #if X64
         internal const bool IS_X64 = true;
